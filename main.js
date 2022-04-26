5b26dafdeb2be27f125c5e5e2a2e6a542ee84a5e
woooo <hug_sheng@qq.com>
2022-04-26 09:42:46 +0800

    生成脚本成功

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, Menu } = require('electron')
+const { app, BrowserWindow, Menu, ipcMain } = require('electron')
 const { SerialPort } = require('serialport')
 const exec = require('child_process').execFile;
 const fs = require('fs');
@@ -14,7 +14,7 @@ reloader(module)
 var mainWindow = null;
 
 // 当所有窗口被关闭了，退出。
-app.on('window-all-closed', function() {
+app.on('window-all-closed', function () {
     // 在 OS X 上，通常用户在明确地按下 Cmd + Q 之前
     // 应用会保持活动状态
     if (process.platform != 'darwin') {
@@ -24,9 +24,17 @@ app.on('window-all-closed', function() {
 
 // 当 Electron 完成了初始化并且准备创建浏览器窗口的时候
 // 这个方法就被调用
-app.on('ready', function() {
+app.on('ready', function () {
     // 创建浏览器窗口。
-    mainWindow = new BrowserWindow({ width: 800, height: 600 });
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
 
     // 加载应用的 index.html
     mainWindow.loadURL('file://' + __dirname + '/src/html/index.html');
@@ -35,80 +43,85 @@ app.on('ready', function() {
     mainWindow.openDevTools();
 
     // 当 window 被关闭，这个事件会被发出
-    mainWindow.on('closed', function() {
+    mainWindow.on('closed', function () {
         // 取消引用 window 对象，如果你的应用支持多窗口的话，
         // 通常会把多个 window 对象存放在一个数组里面，
         // 但这次不是。
         mainWindow = null;
     });
+    var code;
+    ipcMain.on('index.html', (event, arg) => {
+        console.log(arg);
+        code = arg;
+      })
 
     const template = [{
-            label: '文件',
-            submenu: [{
-                    label: '打开文件',
-                },
-                {
-                    label: '保存文件',
-                    click() {
-                        fs.writeFile('./main.py', 'print("你好，世界！")', function(error) {
-                            if (error) {
-                                log.error('write script error')
-                            } else {
-                                log.info('wite script successful')
-                            }
-                        });
-                    }
-                }
-            ]
+        label: '文件',
+        submenu: [{
+            label: '打开文件',
         },
         {
-            label: '设备',
-            submenu: [{
-                    label: '选择设备',
-                    click() {
-                        console.log("test");
-
-                        SerialPort.list().then((ports) => {
-                            console.log(ports); // 打印串口列表
-                        }).catch((err) => {
-                            console.log(err);
-                        });
-
+            label: '保存文件',
+            click() {
+                fs.writeFile('./main.py', code, function (error) {
+                    if (error) {
+                        log.error('write script error')
+                    } else {
+                        log.info('wite script successful')
                     }
-                },
-                {
-                    label: '端口',
-                    // click() {
-                    //     const port = new SerialPort('COM3', (err) => {
-                    //         if (err) {
-                    //             console.log('端口打开失败！');
-                    //             return;
-                    //         }
-                    //         console.log('端口打开成功！');
-                    //     });
-                    // }
-                }
-            ]
-        },
-        {
-            label: '上传',
-            submenu: [{
-                label: '上传程序',
-                click() {
-                    var executablePath = "cli.exe";
-                    var parameters = ["COM3", "main.py"];
-                    exec(executablePath, parameters, function(err, data) {
-                        if (err) {
-                            log.info('uploads error: ', err)
-                            console.error(err);
-                            console.log(data.toString());
-                        } else {
-                            console.log("uploads successly");
-                        }
-                    });
-                }
-            }, ]
+                });
+            }
         }
+        ]
+    },
+    {
+        label: '设备',
+        submenu: [{
+            label: '选择设备',
+            click() {
+                console.log("test");
+
+                SerialPort.list().then((ports) => {
+                    console.log(ports); // 打印串口列表
+                }).catch((err) => {
+                    console.log(err);
+                });
+
+            }
+        },
+        {
+            label: '端口',
+            // click() {
+            //     const port = new SerialPort('COM3', (err) => {
+            //         if (err) {
+            //             console.log('端口打开失败！');
+            //             return;
+            //         }
+            //         console.log('端口打开成功！');
+            //     });
+            // }
+        }
+        ]
+    },
+    {
+        label: '上传',
+        submenu: [{
+            label: '上传程序',
+            click() {
+                var executablePath = "cli.exe";
+                var parameters = ["COM4", "main.py"];
+                exec(executablePath, parameters, function (err, data) {
+                    if (err) {
+                        log.info('uploads error: ', err)
+                        console.error(err);
+                        console.log(data.toString());
+                    } else {
+                        console.log("uploads successly");
+                    }
+                });
+            }
+        },]
+    }
     ];
 
     const appMenu = Menu.buildFromTemplate(template);
